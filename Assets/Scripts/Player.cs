7a261e2122082ab2ce7ae8b024f19b6b159df815
Quentin Leon <Quentin Leon>
2016-10-01 12:57:57 -0700

    started work on collisions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+    public bool dead;
     public float position; // -1 to 1
 
     float speed = 3f;
 
 	// Use this for initialization
 	void Start () {
+        dead = false;
         position = 0;
 	}
 	
@@ -17,5 +19,9 @@ public class Player : MonoBehaviour {
 		int vx = (int)Input.GetAxisRaw ("Horizontal");
 		position += speed * vx * Time.deltaTime;
 		gameObject.transform.position = Vector3.ClampMagnitude(new Vector3(position * 3, 0, 0), 3);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        dead = true;
     }
 }
