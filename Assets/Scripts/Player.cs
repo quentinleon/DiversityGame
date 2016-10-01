03ccfb6763101de9a3768aad498eaa67409aeafd
Edward Zhang <edwardz123@gmail.com>
2016-10-01 13:39:20 -0700

    fix movement and merge

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,30 +4,32 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     public bool dead;
-    public float position; // -1 to 1
+	public Rigidbody2D rb2d;
 
-    float speed = 3f;
+    float speed = 10f;
 
 	// Use this for initialization
 	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
         GetComponent<Animator>().SetBool("dead", false);
         dead = false;
-        position = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (!dead) {
-            int vx = (int)Input.GetAxisRaw("Horizontal");
-            position += speed * vx * Time.deltaTime;
-            gameObject.transform.position = Vector3.ClampMagnitude(new Vector3(position * 3, 0, 0), 3);
-        }
+			int vx = (int)Input.GetAxisRaw ("Horizontal");
+			int vy = (int)Input.GetAxisRaw ("Vertical");
+			rb2d.velocity = new Vector2 (vx, vy) * speed;
+			transform.position = new Vector3 (Mathf.Clamp (transform.position.x, -3, 3), Mathf.Clamp(transform.position.y, -5, 5));
+		}
     }
 
     void OnTriggerEnter2D(Collider2D coll) {
         dead = true;
+		rb2d.isKinematic = false;
         GetComponent<Animator>().SetBool("dead", true);
-        GetComponent<Rigidbody2D>().velocity = new Vector2(5, 7);
+		rb2d.velocity = new Vector2(5, 7);
         print(coll);
     }
 }
