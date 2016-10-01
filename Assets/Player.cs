f43fe561208cf890c449528be5dd5c02f52dfb0c
Quentin Leon <Quentin Leon>
2016-10-01 10:28:54 -0700

    Added player movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,16 +3,26 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-    public int progress;
-    public int position;
+    public float progress; // game lasts 200 seconds
+    public float position; // -1 to 1
+
+    float speed = 3f;
 
 	// Use this for initialization
 	void Start () {
-	
+        position = 0;
+        progress = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        progress += Time.deltaTime;
+        if(Input.GetKey("a") && position > -1){
+            position -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") && position < 1) {
+            position += speed * Time.deltaTime;
+        }
+        gameObject.transform.position = new Vector3(position * 5, 0, 0);
+    }
 }
