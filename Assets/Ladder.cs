f9971146e6d12fbf8c9690eea73104b2227bd8e0
Edward Zhang <edwardz123@gmail.com>
2016-10-01 10:56:22 -0700

    Add scrolling ladder

---
+++ Assets/Ladder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ladder : MonoBehaviour {
+
+	public Transform[] ladderPieces;
+	public float lowerBound;
+	public float upperBound;
+
+	public void Update()
+	{
+		foreach (Transform tr in ladderPieces)
+		{
+			tr.Translate (Vector3.down * 0.1f);
+			if (tr.position.y < lowerBound)
+			{
+				tr.position = new Vector3 (tr.position.x, upperBound);
+			}
+		}
+	}
+}
