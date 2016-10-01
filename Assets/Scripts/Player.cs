b5217a9f4dffe9baa83f31d41e32736c0c4d90b5
Edward Zhang <edwardz123@gmail.com>
2016-10-01 13:27:04 -0700

    add enemy types

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,19 +3,15 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-    public float position; // -1 to 1
-
     float speed = 3f;
 
 	// Use this for initialization
 	void Start () {
-        position = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		int vx = (int)Input.GetAxisRaw ("Horizontal");
-		position += speed * vx * Time.deltaTime;
-		gameObject.transform.position = Vector3.ClampMagnitude(new Vector3(position * 3, 0, 0), 3);
+		int vy = (int)Input.GetAxisRaw ("Vertical");
     }
 }
